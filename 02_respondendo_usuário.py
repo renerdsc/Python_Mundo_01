3084260e5d08003f25a477a8333ce739ae3b6772
Rener Silva <69094616+renerdsc@users.noreply.github.com>
2022-09-13 02:37:45 -0400

    Exercício

---
+++ 02_respondendo_usuário.py
@@ -0,0 +1,5 @@
+nome = str(input('Digite seu nome:'))
+idade = int(input('Qual é sua idade:'))
+estcivil = str(input('Qual é seu estado civil:'))
+endereço = str(input('Digite seu enedereço:'))
+print('É um prazer te conhecer {} voce tem {} anos é {} e mora no endereço {}'.format(nome, idade, estcivil, endereço))
