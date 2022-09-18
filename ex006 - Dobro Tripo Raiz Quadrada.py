1c3499962f38472b353e3d39a6fb6c46898f40e0
Rener Silva <69094616+renerdsc@users.noreply.github.com>
2022-09-18 01:22:17 -0400

    Exercício

---
+++ ex006 - Dobro Tripo Raiz Quadrada.py
@@ -0,0 +1,7 @@
+
+num = int(input('Digite um número:'))
+do = num * 2
+tri = num * 35
+raiz = num ** (1/2)
+print('O dobro de {} vale {}'.format(num, do))
+print('O triplo de {} vale {} \n A raiz quadrada de {} é igual a {:.2f}'.format(num, tri, num, raiz))
