385e462be48223adf978a372aa2895c24ba9969a
Rener Silva <69094616+renerdsc@users.noreply.github.com>
2022-09-17 22:56:58 -0400

    Exercício

---
+++ Tipos Primitivos.py
@@ -0,0 +1,13 @@
+
+'''n1 = int(input('Digite um número:'))
+n2 = int(input('Digite outro número:'))
+soma = n1 + n2
+print('Você digitou o número {} é {} e a soma é {}'.format(n1, n2, soma))'''
+
+
+
+n = input('Digite um valor:')
+print(n.isnumeric()) #verifica se é número 
+
+n = input('Digite algo:')
+print(n.isalpha) #verfica se é letra
